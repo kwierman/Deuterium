d787b8992a20f1db1a9736d2098085a122138ee2
Kevin Wierman <kwierman@gmail.com>
2014-10-16 05:37:17 -0400

    Outlining Logging structure

---
+++ Logging/Logger.hh
@@ -0,0 +1,26 @@
+#ifndef Deuterium_Logger_hh_
+#define Deuterium_Logger_hh_
+
+namespace Deuterium{
+
+	namespace Logging{
+		/*
+		 * HOW TO USE DEUTERIUM LOGGING
+		 * Logging is done in a tree-like structure, which can be accessed statically. This is supposed to replicate
+		 * ~~~~~~~~
+		 *   Deuterium::Logging::GetRootLogger();
+		 *   //or get a logger by tag
+		 *   Deuterium::Logging::GetLoggerByTag("String_Tag");
+		 *   //or get a thread specific logger
+		 *   Deuterium::Logging::GetThreadRootLogger();
+		 * ~~~~~~~~
+		 * HOW TO SET LOGGING FORMAT
+		 * ~~~~~~~~
+		 * 
+		 * ~~~~~~~~
+		 */
+	}
+}
+
+
+#endif //File Guardian
